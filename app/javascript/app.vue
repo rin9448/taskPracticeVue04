6fb7b3b33138ade38d2caa885940ae5847771ee4
rin9448 <rin9448@gmail.com>
2018-10-04 16:33:32 +0900

    html表示部分完成

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,6 +1,38 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <div class="container">
+      <div class="columns">
+        <div class="column status-1">
+          <div class="tags has-addons">
+            <span class="tag">未対応</span>
+            <span class="tag is-dark">{{ tasks.length }}</span>
+          </div>
+          <div class="card" v-for="task in tasks" v-bind:key="task.name">
+            <div class="card-content">{{ task.name }}</div>
+            <div class="card-footer">
+              <div class="card-footer-item">{{ task.assignee }}</div>
+              <div class="card-footer-item">{{ task.mandays }} 人日</div>
+            </div>
+            <div class="card-footer">
+              <a class="card-footer-item">◀︎</a>
+              <a class="card-footer-item">▶︎</a>
+            </div>
+          </div>
+        </div>
+        <div class="column status-2">
+          <div class="tags has-addons">
+            <span class="tag">処理中</span>
+            <span class="tag is-dark"></span>
+          </div>
+        </div>
+        <div class="column status-3">
+          <div class="tags has-addons">
+            <span class="tag">完了</span>
+            <span class="tag is-dark"></span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,7 +40,10 @@
 export default {
   data: function () {
     return {
-      message: "Hello Rintaro!!!!"
+        tasks: [
+          {name: "task2", status: "2", assignee: "🐱", mandays: "4"},
+          {name: "task3", status: "3", assignee: "🐱", mandays: "5"},
+        ],
     }
   }
 }
